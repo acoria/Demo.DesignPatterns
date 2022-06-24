87c877c7921d81feb5adea35a372636a77a808a3
acoria <tewes.verena@gmail.com>
2022-06-24 23:33:57 +0200

    Add singleton kotlin style

---
+++ src/main/kotlin/creational/singleton/kotlinStyle/App.kt
@@ -0,0 +1,6 @@
+package creational.singleton.kotlinStyle
+
+fun main() {
+    HighlanderKotlinStyle.talk()
+    HighlanderKotlinStyle.talk()
+}
---
+++ src/main/kotlin/creational/singleton/kotlinStyle/HighlanderKotlinStyle.kt
@@ -0,0 +1,7 @@
+package creational.singleton.kotlinStyle
+
+object HighlanderKotlinStyle {
+    fun talk() {
+        println("There can be only one! -> I am '$this.'")
+    }
+}
